3d9945bf9ba6fb4ba3ffd98172e83984f4c2df66
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2016-10-25 03:53:01 +0800

    Added index.js to read source files from Google Sheet

---
+++ index.js
@@ -0,0 +1,31 @@
+/*  Download the worksheets from Google Sheet as individual source files.
+gsheets --key=1Spf1qlaAH2hzwa2MvuDNv2pzeSAhlU451IRfEV6stMI --title="all-types.h" --pretty
+ */
+'use strict';
+
+const spreadsheet_key = '1Spf1qlaAH2hzwa2MvuDNv2pzeSAhlU451IRfEV6stMI';
+const filenames = ['all-names.h'];
+
+const fs = require('fs');
+const gsheets = require('gsheets');
+
+for (const filename of filenames) {
+  gsheets.getWorksheet(spreadsheet_key, filename, (err, res) => {
+    if (err) return console.error(err);
+    const lines = [];
+    const data = res.data;
+    let prev_line = null;
+    for (const item of data) {
+      for (const key in item) {
+        const line = item[key];
+        //  Skip multiple blank lines.
+        if (line === '' && line == prev_line)
+          continue;
+        prev_line = line;
+        lines.push(line);
+        console.log(line);
+      }
+      fs.writeFileSync(filename, lines.join('\n'));
+    }
+  });
+}
